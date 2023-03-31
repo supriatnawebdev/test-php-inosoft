10e86d19845c340acfa6c209fe1797f7be5c1ee8
Yusup Supriatna <yusupsupriatna442@gmail.com>
2023-03-31 13:38:40 +0700

    next commit

--- index.php
+++ index.php
@@ -1,19 +1,33 @@
 <?php
-// Fungsi untuk menghitung luas lingkaran
 function luas_lingkaran($jari_jari) {
-    $luas = pi() * $jari_jari * $jari_jari;
-    return $luas;
+  $luas = 3.14 * pow($jari_jari, 2);
+  return number_format((float)$luas, 2, '.', '');
 }
 
-// Perulangan dari 1 hingga 100
+function keliling_lingkaran($jari_jari) {
+  $keliling = 2 * 3.14 * $jari_jari;
+  return number_format((float)$keliling, 2, '.', '');
+}
+
+function luas_persegi($panjang, $lebar) {
+  $luas = $panjang * $lebar;
+  return number_format((float)$luas, 2, '.', '');
+}
+
 for ($i = 1; $i <= 100; $i++) {
-    // Cek apakah $i adalah kelipatan 3
-    if ($i % 3 == 0) {
-        // Hitung jari-jari lingkaran dengan membagi $i dengan 3
-        $jari_jari = $i / 3;
-        // Hitung luas lingkaran dan cetak hasilnya
-        $luas = luas_lingkaran($jari_jari);
-        echo "Luas lingkaran dengan jari-jari $jari_jari adalah: $luas<br>";
-    }
+  if ($i % 3 == 0 && $i % 5 == 0) {
+    $luas_persegi = luas_persegi($i/3, $i/5);
+    echo "Angka $i adalah kelipatan 3 dan 5, luas persegi dengan panjang $i/3 dan lebar $i/5 adalah: $luas_persegi <br>";
+  } elseif ($i % 3 == 0) {
+    $jari_jari = $i/3;
+    $luas_lingkaran = luas_lingkaran($jari_jari);
+    echo "Angka $i adalah kelipatan 3, luas lingkaran dengan jari-jari $jari_jari adalah: $luas_lingkaran <br>";
+  } elseif ($i % 5 == 0) {
+    $jari_jari = $i/5;
+    $keliling_lingkaran = keliling_lingkaran($jari_jari);
+    echo "Angka $i adalah kelipatan 5, keliling lingkaran dengan jari-jari $jari_jari adalah: $keliling_lingkaran <br>";
+  } else {
+    echo "Angka $i <br>";
+  }
 }
 ?>
