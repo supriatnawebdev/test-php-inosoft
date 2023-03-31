0c864d3ffeab2741675de394fe95ea0d0e66e015
Yusup Supriatna <yusupsupriatna442@gmail.com>
2023-03-31 13:28:30 +0700

    second commit

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
-<?php 
+<?php
+// Fungsi untuk menghitung luas lingkaran
+function luas_lingkaran($jari_jari) {
+    $luas = pi() * $jari_jari * $jari_jari;
+    return $luas;
+}
 
-
+// Perulangan dari 1 hingga 100
 for ($i = 1; $i <= 100; $i++) {
-    echo $i . '<br>';
-  }
-
-
-
+    // Cek apakah $i adalah kelipatan 3
+    if ($i % 3 == 0) {
+        // Hitung jari-jari lingkaran dengan membagi $i dengan 3
+        $jari_jari = $i / 3;
+        // Hitung luas lingkaran dan cetak hasilnya
+        $luas = luas_lingkaran($jari_jari);
+        echo "Luas lingkaran dengan jari-jari $jari_jari adalah: $luas<br>";
+    }
+}
 ?>
